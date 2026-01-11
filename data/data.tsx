03fa52b16f1f4bffa8827b1aba3eb7b7607027c7
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2026-01-11 13:57:30 +0530

    changed resume

--- data/data.tsx
+++ data/data.tsx
@@ -23,7 +23,7 @@ export const DATA = {
   projects: projects,
   blogs: blogs,
   resume:
-    "https://drive.google.com/file/d/1n7WIVZN19CO9qsLG-_kMWysXnWlEGKm8/view?usp=drive_link",
+    "https://drive.google.com/file/d/1bHovkumxuTYBJP3A9QJgr_i1ba3o_HTb/view?usp=drive_link",
   socials: {
     github: "https://github.com/amandevopsreal",
     linkedin: "https://www.linkedin.com/in/aman-kumar-mishra-97b841226/",
